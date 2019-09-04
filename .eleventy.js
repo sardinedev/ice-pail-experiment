545bfc3d46e7619c9b1800dcdf0b7604e2034de4
Hugo Nogueira <hugo.nogueira@uk.dk.com>
2019-09-04 11:06:32 +0100

    working 11ty config

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+  // Input directory: src
+  // Output directory: _site
+
+  // The following copies to `_site/img`
+  eleventyConfig.addPassthroughCopy('site/img');
+  eleventyConfig.addPassthroughCopy('site/css');
+};
