ee049c7af1fce92b08b23b820c5c867fb899f745
Hugo Nogueira <hugo.nogueira@uk.dk.com>
2019-10-26 15:53:06 +0100

    initial rollup integration

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const extensions = ['.js', '.ts'];
+
+export default async function({ watch }) {
+  return {
+    input: 'src/lazy-picture.ts',
+    output: {
+      file: 'lib/lazy-picture-esm.js',
+      format: 'esm',
+      assetFileNames: '[name]-[hash][extname]'
+    },
+    watch: {
+      clearScreen: false,
+      // Avoid watching intermediate files, else watch gets stuck in a loop.
+      // 11ty source files are watched by eleventyPlugin.
+      exclude: '.build-tmp/**/*.html'
+    },
+    plugins: [
+      resolve({ extensions }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**' // only transpile our source code
+      }),
+      terser({ ecma: 8, module: true })
+    ]
+  };
+}
