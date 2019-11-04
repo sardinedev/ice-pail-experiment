40ff30dd5ceadcb7c64e07862c0584d6a9234634
Hugo Nogueira <hnogueira@marabyte.com>
2019-11-04 15:56:13 +0000

    Added posts updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
 const sassBuild = require('./lib/sass.11ty');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Input directory: src
   // Output directory: _site
 
+  // Add filters to Nunjucks
+  eleventyConfig.addFilter("date", require("./site/_filters/date.11ty"));
   eleventyConfig.addFilter('sass', sassBuild);
 
   // The following copies to `_site/assets`
   eleventyConfig.addPassthroughCopy('site/assets');
-
+  eleventyConfig.addPassthroughCopy('site/uploads');
   eleventyConfig.addPassthroughCopy('admin');
 };
