f7d9122e456acabf7933cbaa02e2ea24554024db
Hugo Nogueira <hugo.nogueira@lego.com>
2022-02-28 14:05:57 +0000

    chore: clean up

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,44 @@
-const sassBuild = require('./lib/sass.11ty');
-const tailwindBuild = require('./lib/tailwind.11ty');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
-const htmlmin = require('./lib/html-minify.11ty');
-const dateFormat = require('./lib/date.11ty');
-const eleventyRollup = require('./lib/rollup.11ty');
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const safeLinks = require("@sardine/eleventy-plugin-external-links");
+const tinyHTML = require("@sardine/eleventy-plugin-tinyhtml");
+const tinyCSS = require("@sardine/eleventy-plugin-tinycss");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const dateFormat = require("./lib/date.11ty");
+
+const purgeFromTailwind = content => content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+const markdownLibrary = markdownIt({ html: true }).use(markdownItAnchor, {
+  permalink: markdownItAnchor.permalink.headerLink()
+});
 
 module.exports = function (eleventyConfig) {
-  // Input directory: src
-  // Output directory: _site
+  // Config `markdownIt` to use the `markdown-it-anchor` plugin
+  // This is a custom plugin that generates an anchor link for each header
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
 
   // Add filters to Nunjucks
-  eleventyConfig.addFilter('date', dateFormat);
-  eleventyConfig.addFilter('sass', sassBuild);
-  eleventyConfig.addNunjucksAsyncFilter('rollup', eleventyRollup);
-  eleventyConfig.addNunjucksAsyncFilter('tailwind', tailwindBuild);
+  eleventyConfig.addFilter("date", dateFormat);
 
   // The following copies to `_site/assets`
-  eleventyConfig.addPassthroughCopy('site/assets');
-  eleventyConfig.addPassthroughCopy('site/uploads');
-
-  // eleventyConfig.addTransform('htmlmin', htmlmin);
+  eleventyConfig.addPassthroughCopy("site/assets");
+  eleventyConfig.addPassthroughCopy("site/uploads");
 
   eleventyConfig.addPlugin(pluginRss);
+
+  if (process.env.ELEVENTY_ENV === "production") {
+    eleventyConfig.addPlugin(tinyHTML);
+    eleventyConfig.addPlugin(safeLinks);
+    eleventyConfig.addPlugin(tinyCSS, {
+      purgeCSS: {
+        keyframes: true,
+        extractors: [
+          {
+            extractor: purgeFromTailwind,
+            extensions: ["html"]
+          }
+        ]
+      }
+    });
+  }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  theme: {
-    extend: {
-      height: {
-        "screen-w": "100vw",
-        "screen-w-1/2": "50vw",
-        "screen-w-1/3": "calc(100vw / 3)",
-        "screen-w-1/4": "25vw",
-        "screen-w-1/5": "20vw",
-      },
-      screens: {
-        xxl: "1480px",
-      },
-    },
-  },
+  content: [
+    './site/**/*.{html,njk}',
+  ],
 };
